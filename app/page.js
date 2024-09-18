bf3d99c605b67d112c342d63872e41b7ce399902
Deepansu <itzdeepansu@gmail.com>
2024-09-18 14:25:14 +0530

    saver

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
-// import SideBar from "@/components/standalone/SideBar";
-// import NavBar from "@/components/standalone/NavBar";
-// import RoutesTable from "@/components/standalone/RoutesTable";
+
 export default function Home() {
   return (
     // <div className="flex min-h-screen w-full flex-col bg-muted/40 bg-black text-white">
